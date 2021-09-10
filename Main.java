272d61bb43223804044459e2995547d292810a51
RizwanSyedV2 <>
2021-09-10 13:47:52 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+class Main {
+  public static void main(String[] args) {
+
+
+	
+		Scanner kb = new Scanner( System.in );
+		System.out.print( "Enter the base1 :: " );
+		int base1 = kb.nextInt();
+		System.out.print( "Enter the base2 :: " );		
+		int base2 = kb.nextInt();
+		System.out.print( "Enter the height :: " );		
+		int height = kb.nextInt();
+		
+		//print out the answer here
+	System.out.println("helloooo"); 
+	}
+}
+
+/*
+
+Sample Data: 
+3 3 3
+5 6 7
+7 10 6
+13 9 3
+6 11 4
+11 8 5
+
+
+Sample Output : 
+9.0
+38.5
+51.0
+33.0
+34.0
+47.5
+
+// anyone know what to do?
+//Fork the project and uh... Print out the answer ig. Doesn't look too ugly. 
+
+*/
